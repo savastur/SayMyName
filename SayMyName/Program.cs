c4176f5fb1705197593c6e916035a96213ff4cd6
Sturm <savagesturm@gmail.com>
2026-01-30 08:55:36 -0700

    Built case structures with names and added while loop.

--- SayMyName/Program.cs
+++ SayMyName/Program.cs
@@ -1,4 +1,8 @@
-﻿namespace SayMyName
+﻿using System;
+using System.Linq.Expressions;
+using System.Net.Security;
+
+namespace SayMyName
 //Sturm Savage
 //RCET2265
 //Spring Semester
@@ -9,14 +13,59 @@
         static void Main(string[] args)
         {
             String Userinput;
+            bool isvalid = false;
 
-            Console.WriteLine("Please write your name:");
-            Userinput = Console.ReadLine();
+            do
+            {
+               
+                Console.WriteLine("Please write your name:");
+                Console.WriteLine("Enter \"Q\" to quit.");
+                 Userinput = Console.ReadLine();
+                
+                            
 
-            Console.WriteLine("Hello " + Userinput + "!!!!");
+                if (Userinput == "q" || Userinput == "Q")
+                {
+                    isvalid= true;
+                }
+
+                
+
+                else
+                {
+                    switch (Userinput)
+                    {
+                        case "Joe":
+                            Console.WriteLine($"Hello {Userinput}!!!!");
+                            break;
+                        case "Emily":
+                            Console.WriteLine($"Hello {Userinput}!!!!");
+                            break;
+                        case "Sturm Savage":
+                            Console.WriteLine($"Hello {Userinput}. How can I help you?");
+                            break;
+                        case "q":
+                            isvalid = true;
+                            break;
+                        case "Q":
+                            isvalid= true;
+                            break;
 
 
 
+                    }
+                    if (Userinput != "Joe , Emily , Sturm Savage")
+                    {
+                        Console.WriteLine($"Hello {Userinput}!!!!");
+
+                    }
+
+                }
+            
+            } 
+            while (!isvalid);
+            
+            
 
 
             Console.Read();
